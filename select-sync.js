9736ac86a26c5036f34a2891ecf8cb980671ab26
Victor <me@bjatta.ru>
2017-07-08 12:03:52 +0300

    HTML for git pages

---
+++ select-sync.js
@@ -0,0 +1,50 @@
+;((container,maxItemNumber)=>{
+    if(container){
+
+        let _=container;
+        let __=console.log;
+
+        let selectListChecker = e=>{
+            let selects=_.querySelectorAll('select');
+            let selected=[];
+
+            selects.forEach(
+                select=>{
+                    if (selected.indexOf(select.value)!==-1){
+                        select.value="";
+                    }
+                        select.querySelectorAll('option').forEach(
+                        op=>{
+                            if (op.selected && op.value) {
+                                selected.push(op.value);
+                            }
+                            if (selected.indexOf(op.value)!==-1){
+                                op.setAttribute('hidden','');
+                            }else{
+                                op.removeAttribute('hidden');
+                            }
+                        });
+                });
+        };
+
+        document.addEventListener("DOMContentLoaded", e=>{
+
+            firstSelect = _.querySelector('select');
+            firstSelect.addEventListener('change',selectListChecker);
+            _.querySelector('#select-sync-button').addEventListener('click',e=>{
+
+                if (_.querySelectorAll('select').length<maxItemNumber) {
+                    let clonedElement = firstSelect.cloneNode(true);
+
+                    _.insertBefore(clonedElement, _.lastChild);
+                    clonedElement.addEventListener('change',selectListChecker);
+                }else{
+                    __('Max number of items : '+maxItemNumber+' are reached!');
+                }
+            });
+
+        });
+    }else{
+        console.error('Container with id="#select-sync" not found. Try to correct or html or given id.');
+    }
+})(document.querySelector('#select-sync'),6);
